085a1cbd2164029d80180875d28c443aa0b03429
Koushik Mallik <koushikmallik001@gmail.com>
2025-01-19 01:09:10 +0530

    cleaned up

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const messageRoutes = require('./src/routes/messageRoutes')
 const setupSocket = require('./src/utils/socket')
 
 dotenv.config()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 const app = express()
 const httpServer = createServer(app)
@@ -48,5 +48,5 @@ setupSocket(io)
 
 // Start server
 httpServer.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+    console.log(`Server running on ${PORT}`)
 })
